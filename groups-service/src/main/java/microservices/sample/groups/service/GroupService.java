19eaaf95f5f7f8e576dd22bf554009609fdafe14
Matías Hermosilla <matiasahermosilla@gmail.com>
2021-09-06 00:09:50 -0300

    Create GroupService.java

---
+++ groups-service/src/main/java/microservices/sample/groups/service/GroupService.java
@@ -0,0 +1,43 @@
+package microservices.sample.groups.service;
+
+import com.querydsl.core.group.Group;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import microservices.sample.groups.repository.GroupRepository;
+
+/**
+ * Group service.
+ * 
+ * @author Matías Hermosilla
+ * @since 06-09-2021
+ */
+@Service
+public class GroupService {
+    
+    /**
+     * Group repository.
+     */
+    @Autowired
+    private GroupRepository groupRepository;
+
+    /**
+     * Saves a group.
+     * 
+     * @param group Group to save.
+     */
+    public void save(Group group) {
+        // Save object to database
+        this.groupRepository.save(group);
+    }
+
+    /**
+     * Deletes a group.
+     * 
+     * @param group Group to delete.
+     */
+    public void delete(Group group) {
+        // Delete object from database
+        this.groupRepository.delete(group);
+    }
+
+}
