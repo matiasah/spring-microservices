e4eb469f199f55a61f9cf8e43cf2ffccff511c47
Matías Hermosilla <matiasahermosilla@gmail.com>
2021-09-06 00:09:27 -0300

    Add missing @Service annotation on ApplicationService

--- clients-service/src/main/java/microservices/sample/clients/service/ApplicationService.java
+++ clients-service/src/main/java/microservices/sample/clients/service/ApplicationService.java
@@ -1,6 +1,7 @@
 package microservices.sample.clients.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import microservices.sample.clients.model.Application;
 import microservices.sample.clients.repository.ApplicationRepository;
 
@@ -10,6 +11,7 @@ import microservices.sample.clients.repository.ApplicationRepository;
  * @author Matías Hermosilla
  * @since 05-09-2021
  */
+@Service
 public class ApplicationService {
 
     /**
