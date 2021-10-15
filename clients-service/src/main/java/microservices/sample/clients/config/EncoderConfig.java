f82856bb5199d5578622bccd6b6395594f0a743a
Matías Hermosilla <matiasahermosilla@gmail.com>
2021-10-14 23:28:20 -0300

    Create EncoderConfig.java

---
+++ clients-service/src/main/java/microservices/sample/clients/config/EncoderConfig.java
@@ -0,0 +1,20 @@
+package microservices.sample.clients.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Matías Hermosilla
+ * @since 11-10-2021
+ */
+@Configuration
+public class EncoderConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
